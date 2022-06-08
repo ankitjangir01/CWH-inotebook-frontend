987ff46bc463b936b202fbf9b2bc3dbf3092e260
Ankit Jangir <ankitjangirnice@gmail.com>
2022-06-08 12:40:54 +0530

    setting up routes and components Home, About, Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my react app</h1>
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/about' element={<About/>} />
+      </Routes>
+    </>
   );
 }
 
